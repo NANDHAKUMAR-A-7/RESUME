bcfb156576e566c0cb37c5723e8cc3e2418ede77
Nandhakumar A <fdemo1177@gmail.com>
2025-07-20 00:55:03 +0530

    html,css,js files are uploaded

---
+++ script.js
@@ -0,0 +1,6 @@
+// alert("Welcome to Nandhakumar A 's Resume");
+
+// const linkedin = document.getElementById("linkedinbtn");
+//     linkedin.addEventListener("mouseover" , function(){
+//     alert("Redirect to Linkedin");
+// });
