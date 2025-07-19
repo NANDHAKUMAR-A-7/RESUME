356c4636ee02015c32d5b260e67e203ad1272ef3
Nandhakumar A <fdemo1177@gmail.com>
2025-07-19 23:10:59 +0530

    resume

--- Users/nathi/OneDrive/Desktop/Resume/script.js
+++
@@ -1,6 +0,0 @@
-// alert("Welcome to Nandhakumar A 's Resume");
-
-// const linkedin = document.getElementById("linkedinbtn");
-//     linkedin.addEventListener("mouseover" , function(){
-//     alert("Redirect to Linkedin");
-// });
