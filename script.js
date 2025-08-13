0bba54db4b5b96072085508082b733fe27ef2deb
NANDHAKUMAR-A-7 <nandhainfom@gmail.com>
2025-08-13 20:30:50 +0530

    added hardware file as website

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-alert("Welcome to Nandhakumar A 's Resume");
+// alert("Welcome to Nandhakumar A 's Resume");
 
 // const linkedin = document.getElementById("linkedinbtn");
 //     linkedin.addEventListener("mouseover" , function(){
 //     alert("Redirect to Linkedin");
 // });
+const hardware = document.getElementById("hardwareBtn");
+function travel(){
+     let a = confirm("Look for Hardware");
+    {
+        if(a == true){
+            alert("Redirecting");
+            window.location.href = "Hardware.html";
+        }else{
+            alert("Thank you");
+        }
+    }
+}
