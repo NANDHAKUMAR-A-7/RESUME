8f9b18d36555e0c02e591ec89c9c1788a83a7703
NANDHAKUMAR-A-7 <nandhainfom@gmail.com>
2026-02-09 12:54:07 +0530

    updated resume

---
+++ Users/nathi/Downloads/RESUME-main (1)/RESUME-main/Hardware.js
@@ -0,0 +1,9 @@
+// Optional: Smooth Scroll Enhancement
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
---
+++ Users/nathi/Downloads/Resume-main/script.js
@@ -0,0 +1,6 @@
+ alert("Nandhakumar A 's Resume");
+
+// const linkedin = document.getElementById("linkedinbtn");
+//     linkedin.addEventListener("mouseover" , function(){
+//     alert("Redirect to Linkedin");
+// });
